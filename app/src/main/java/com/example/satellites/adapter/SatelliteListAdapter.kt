aabbf3044e6ac2c5fe9468b2e82bc11a5b2f3d0c
Sertac Okan Celik <sertacokang@gmail.com>
2023-02-12 17:14:50 +0300

    Create SatelliteListAdapter.kt

---
+++ app/src/main/java/com/example/satellites/adapter/SatelliteListAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.satellites.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.example.satellites.database.list.SatelliteEntity
+import com.example.satellites.holder.SatelliteListHolder
+
+class SatelliteListAdapter : ListAdapter<SatelliteEntity, SatelliteListHolder>(SatelliteEntityDiffUtil) {
+
+    private companion object SatelliteEntityDiffUtil : DiffUtil.ItemCallback<SatelliteEntity>() {
+        override fun areItemsTheSame(oldItem: SatelliteEntity, newItem: SatelliteEntity): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: SatelliteEntity,
+            newItem: SatelliteEntity
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SatelliteListHolder {
+        return SatelliteListHolder.create(parent)
+    }
+
+    override fun onBindViewHolder(holder: SatelliteListHolder, position: Int) {
+        val satelliteEntity = getItem(position)
+        holder.bind(satelliteEntity)
+    }
+}
