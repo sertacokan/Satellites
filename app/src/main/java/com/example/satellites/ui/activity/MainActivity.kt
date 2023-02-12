601cea59c694c7526b3603b0b1838f754dcf2d28
Sertac Okan Celik <sertacokang@gmail.com>
2023-02-12 22:34:15 +0300

    Create nav_graph.xml

--- app/src/main/java/com/example/satellites/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/satellites/ui/activity/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.example.satellites
+package com.example.satellites.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.satellites.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
