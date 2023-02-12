9815b92a8aa991f0f43aecde90cb0e739755ce05
Sertac Okan Celik <sertacokang@gmail.com>
2023-02-12 22:22:09 +0300

    Fix position request method return type

--- app/src/main/java/com/example/satellites/network/MockService.kt
+++ app/src/main/java/com/example/satellites/network/MockService.kt
@@ -3,7 +3,7 @@ package com.example.satellites.network
 import com.example.satellites.network.adapter.NetworkState
 import com.example.satellites.network.model.response.Satellite
 import com.example.satellites.network.model.response.SatelliteDetail
-import com.example.satellites.network.model.response.SatellitePosition
+import com.example.satellites.network.model.response.SatellitePositionListItem
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -12,8 +12,8 @@ interface MockService {
     @GET("list")
     suspend fun fetchSatellites(): NetworkState<List<Satellite>?>
 
-    @GET("position/{id}")
-    suspend fun fetchPositions(@Path("id") id: Int): NetworkState<SatellitePosition?>
+    @GET("positions/{id}")
+    suspend fun fetchPositions(@Path("id") id: Int): NetworkState<SatellitePositionListItem?>
 
     @GET("detail/{id}")
     suspend fun fetchDetail(@Path("id") id: Int): NetworkState<SatelliteDetail?>
